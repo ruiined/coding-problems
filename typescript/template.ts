da2c66984a154ca06fbd828483397d943dd52256
Maria G <ruiined@gmail.com>
2024-10-25 17:08:38 +0100

    feat: typescript template

---
+++ typescript/template.ts
@@ -0,0 +1,20 @@
+import { strict as assert } from "assert";
+
+/* 
+
+
+
+*/
+
+const func = () => {
+  return;
+};
+
+/*
+  Time complexity: 
+  Space complexity: 
+*/
+
+assert.deepEqual(func(), "");
+assert.deepEqual(func(), "");
+assert.deepEqual(func(), "");
